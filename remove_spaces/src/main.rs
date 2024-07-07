467c82951073751c384beae74b030ce2835d3f2c
Mack Octavian <maestrooctavian@gmail.com>
2024-07-08 02:57:50 +0300

    Added a program that removes whitespace from a given string

---
+++ remove_spaces/src/main.rs
@@ -0,0 +1,8 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+fn main() {
+    no_space(String::from("Mack Octavian Amandus"));
+}
+
+fn no_space(x: String) -> String {
+    x.split_whitespace().collect()
+}
