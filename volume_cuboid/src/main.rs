18d8535d3f872ba313d8052e7152390c53f8b317
Mack Octavian <maestrooctavian@gmail.com>
2024-07-08 03:04:00 +0300

    Added a program to calculate volume of a cuboid

---
+++ volume_cuboid/src/main.rs
@@ -0,0 +1,10 @@
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length,
+//width and height of the cuboid. Write a function to help Bob with this calculation.
+fn main() {
+    println!("Hello, world!");
+}
+
+fn get_volume_of_cuboid(length: f32, width: f32, height: f32) -> f32 {
+    // your code here
+    length * width * height
+}
