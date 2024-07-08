20f2f7311d86e971957755ef0847ced3cf453bfa
Mack Octavian <maestrooctavian@gmail.com>
2024-07-08 03:14:17 +0300

    Added a function that converts boolean to it's string representation

---
+++ bool_to_string/src/main.rs
@@ -0,0 +1,9 @@
+//Implement a function which convert the given boolean value into its string representation.
+fn boolean_to_string(b: bool) -> String {
+    if b == true {
+        return String::from("true");
+    } else {
+        return String::from("false");
+    }
+}
+fn main() {}
